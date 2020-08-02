7d16b82af0b43c624e7c8642601970e8d18c5579
rromikas <rromikas@gmail.com>
2020-08-02 21:12:38 +0300

    ok

--- src/components/user/Home.jsx
+++ src/components/user/Home.jsx
@@ -124,7 +124,7 @@ const Home = ({ publicUsers, ratings, user }) => {
                 <div
                   className="row no-gutters mb-2"
                   style={{
-                    fontSize: "calc(1.6em + 2vw)",
+                    fontSize: "calc(1.6em + 1vw)",
                     fontWeight: "600",
                   }}
                 >
